9cc58670a64fc6eb2a42fab811ac4c849b468945
Philipp Friese <philipp.friese@tum.de>
2021-12-11 08:55:54 +0100

    eleventh challenge

---
+++ 11-eleventh/benches/benchmark.rs
@@ -0,0 +1,26 @@
+use std::time::Duration;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use std::fs;
+
+use eleventh::eleventh::first;
+use eleventh::eleventh::second;
+
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("eleventh");
+    let data = fs::read_to_string("data").expect("Oops");
+
+    group.sample_size(1000);
+    group.measurement_time(Duration::from_secs(5));
+
+    group.bench_function("first", |b| b.iter(|| first(&data)));
+    group.bench_function("second", |b| b.iter(|| second(&data)));
+
+
+    group.finish();
+
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ 11-eleventh/src/eleventh.rs
@@ -0,0 +1,140 @@
+pub fn first(content: &str) -> usize {
+    const SIZE: usize = 10;
+    let mut data = [[0;SIZE+2];SIZE+2];
+    
+    content
+        .split("\n")
+        .enumerate().for_each(|(i,row)| {
+            let mut char_iter = row.chars();
+            data[i+1] = [
+                0, 
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                0
+            ];
+
+        });
+
+    let mut flashes = 0;
+
+    for _ in 0..100 {
+        // first: increase energy levels
+        data.iter_mut().for_each(|r| r.iter_mut().for_each(|el| *el+=1));
+        
+
+        // second: flash
+        let mut changed = true;
+        let mut flashers = Vec::new();
+        while changed {
+            changed = false;
+
+            for i in 1..SIZE+1 {
+                for j in 1..SIZE+1 {
+                    if data[i][j] > 9 && !flashers.contains(&(i,j)) {
+                        changed = true;
+
+                        flashers.push((i,j));
+
+                        data[i-1][j] += 1;
+                        data[i+1][j] += 1;
+
+                        data[i][j-1] += 1;
+                        data[i][j+1] += 1;
+
+                        data[i-1][j-1] += 1;
+                        data[i+1][j+1] += 1;
+
+                        data[i+1][j-1] += 1;
+                        data[i-1][j+1] += 1;
+                    }
+                }
+            }
+        }
+
+        // finally: reset flashers
+        flashes += flashers.len();
+        flashers.iter().for_each(|(i,j)| data[*i][*j] = 0);
+    }
+
+    flashes
+}
+
+pub fn second(content: &str) -> u64 {
+    const SIZE: usize = 10;
+    let mut data = [[0;SIZE+2];SIZE+2];
+    
+    content
+        .split("\n")
+        .enumerate().for_each(|(i,row)| {
+            let mut char_iter = row.chars();
+            data[i+1] = [
+                0, 
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                char_iter.next().unwrap().to_digit(10).unwrap(),
+                0
+            ];
+
+        });
+
+    let mut flash_step = 0;
+
+    let mut run = true;
+    while run  {
+        flash_step += 1;
+        // first: increase energy levels
+        data.iter_mut().for_each(|r| r.iter_mut().for_each(|el| *el+=1));
+        
+
+        // second: flash
+        let mut changed = true;
+        let mut flashers = Vec::new();
+        while changed {
+            changed = false;
+
+            for i in 1..SIZE+1 {
+                for j in 1..SIZE+1 {
+                    if data[i][j] > 9 && !flashers.contains(&(i,j)) {
+                        changed = true;
+
+                        flashers.push((i,j));
+
+                        data[i-1][j] += 1;
+                        data[i+1][j] += 1;
+
+                        data[i][j-1] += 1;
+                        data[i][j+1] += 1;
+
+                        data[i-1][j-1] += 1;
+                        data[i+1][j+1] += 1;
+
+                        data[i+1][j-1] += 1;
+                        data[i-1][j+1] += 1;
+                    }
+                }
+            }
+        }
+
+        run = flashers.len() != SIZE*SIZE;
+        
+        // finally: reset flashers
+        flashers.iter().for_each(|(i,j)| data[*i][*j] = 0);
+    }
+
+    flash_step
+}
